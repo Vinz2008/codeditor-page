316bab6f7300de42028d1a8a2959efe4ebf9e105
Vinz2008 <clubfunreading@@gmail.com>
2021-12-02 22:45:20 +0100

    added creating file and execute it in python backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,24 @@
 from flask import Flask, jsonify, request, render_template
+import os
+import subprocess
 app = Flask(__name__)
-
+incoming = ""
 @app.route("/python", methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
         print('Incoming..')
-        incoming = request.get_json()
+        json_incoming = request.get_json()
+        incoming = json.dumps(json_incoming)
+        print(incoming)
+        f = open('scripts.py', 'rw')
+        f.write(incoming)
+        f.close()
+        try:
+            outcoming = subprocess.check_output(['python3','scripts.py'])
+        except subprocess.CalledProcessError as err:
+            print(err)
     else:
-        message = {'greeting':'Hello from Flask!'}
+        if len(incoming) != 0:
+            message = incoming
         return jsonify(message)
 # https://python-interpreter1.herokuapp.com/
