2acb6006a8da234624b22c959c9f51413fc3b69e
Vinz2008 <clubfunreading@gmail.com>
2021-12-07 20:36:12 +0100

    added gevent for backend

--- backend/backend-setup.py
+++ backend/backend-setup.py
@@ -1,4 +1,6 @@
+from gevent import monkey
+monkey.patch_all()
 from gevent.pywsgi import WSGIServer
 from app import python_requests
-http_server = WSGIServer(('', 5000), python_requests())
+http_server = WSGIServer(('0.0.0.0', 5000), python_requests())
 http_server.serve_forever()
