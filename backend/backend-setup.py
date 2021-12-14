0ad62242287d719a02c2ef4ca5024fa87bf72631
Vinz2008 <clubfunreading@gmail.com>
2021-12-14 17:00:47 +0100

    removed function in app.py

--- backend/backend-setup.py
+++
@@ -1,6 +0,0 @@
-from gevent import monkey
-monkey.patch_all()
-from gevent.pywsgi import WSGIServer
-from app import python_requests
-http_server = WSGIServer(('0.0.0.0', 5000), python_requests())
-http_server.serve_forever()
