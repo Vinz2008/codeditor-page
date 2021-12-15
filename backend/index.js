bcda18026f66fb5fe4d9daceec9acbc04dafd639
Ubuntu <ubuntu@ip-172-31-32-14.eu-west-3.compute.internal>
2021-12-15 16:16:20 +0000

    continued nodejs

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
-const { exec } = require("child_process")
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require("child_process");
+const app = express();
+const port = 8000;
+
+
 var fs = require('fs');
 const { stdout, stderr } = require("process");
 function run(code) {
-fs.writeFile('script.py','', function (err) {
+fs.writeFile('script.py',code, function (err) {
     if (err) throw err; {
-    console.log("")
+    console.log("");
     }
 });
 exec("python3 script.py", (err, stdout, stderr) => {
     var output = stdout
+    console.log(output)
+    app.get('/', (req, res) => res.send(output));
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 })
 }
+
+run("print('This is my api')")
 
