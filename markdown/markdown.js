47443042407f77b3fb5cc48fb711ab033a7a83a2
Vinz2008 <clubfunreading@gmail.com>
2022-01-19 18:46:28 +0100

    started theming ace dark and light mode

--- markdown/markdown.js
+++ markdown/markdown.js
@@ -1,6 +1,31 @@
+var theme = localStorage.getItem("theme");
+function setThemeAce() {
+    var theme = localStorage.getItem("theme");
+    if (theme == "light") {
+        editor.setTheme("ace/theme/github");
+    } else if (theme == "dark") {
+        editor.setTheme("ace/theme/dracula");
+    }
+}
+
 var editor = ace.edit("mycode");
-editor.setTheme("ace/theme/dracula");
+editor.setTheme("ace/theme/github");
+
+var buttonTheme = document.getElementById('btn-toggle')
+setThemeAce()
+
+buttonTheme.addEventListener("click", function() {
+    setThemeAce();
+
+});
+
 editor.session.setMode("ace/mode/markdown");
+editor.setOptions({
+    maxLines: 33,
+    minLines: 28,
+    autoScrollEditorIntoView: true,
+    keyboardHandler: null,
+})
 editor.resize()
 
 var myCode = editor.getSession().getValue();
